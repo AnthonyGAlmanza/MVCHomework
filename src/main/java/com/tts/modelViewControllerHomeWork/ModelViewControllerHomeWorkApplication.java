d702d42259f2caa14a4940848c4c9d3212d8ad3a
Anthony <antman_2012@yahoo.com>
2021-02-23 15:48:50 -0600

    initial commit

---
+++ src/main/java/com/tts/modelViewControllerHomeWork/ModelViewControllerHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.tts.modelViewControllerHomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelViewControllerHomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelViewControllerHomeWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/modelViewControllerHomeWork/ModelViewControllerHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.modelViewControllerHomeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelViewControllerHomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
